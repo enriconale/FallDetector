1d6e77029cd19eb711002bb958afb89af8da58e1
enriconale <developer.nalettoe@yahoo.it>
2015-04-09 11:05:10 +0200

    Added a delete button in SessionDetailsActivity

--- app/src/main/java/it/unipd/dei/esp1415/thetrumannshow/FallDetector/SessionDetailsActivity.java
+++ app/src/main/java/it/unipd/dei/esp1415/thetrumannshow/FallDetector/SessionDetailsActivity.java
@@ -1,5 +1,6 @@
 package it.unipd.dei.esp1415.thetrumannshow.FallDetector;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -16,14 +17,15 @@ public class SessionDetailsActivity extends ActionBarActivity {
     private Session mSession;
     private TextView mSessionName;
     private TextView mCreationDate;
+    private int mPositionInList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session_details);
 
-        int positionInList = getIntent().getExtras().getInt(SessionsListAdapter.SESSION_DETAILS);
-        mSession = SessionsLab.get(getApplicationContext()).getSessions().get(positionInList);
+        mPositionInList = getIntent().getExtras().getInt(SessionsListAdapter.SESSION_DETAILS);
+        mSession = SessionsLab.get(getApplicationContext()).getSessions().get(mPositionInList);
 
         mSessionName = (TextView)findViewById(R.id.session_name);
         mCreationDate = (TextView)findViewById(R.id.date_time);
@@ -47,10 +49,11 @@ public class SessionDetailsActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_delete_session:
+                SessionsLab.get(getApplicationContext()).getSessions().remove(mPositionInList);
+                NavUtils.navigateUpFromSameTask(this);
+                break;
         }
 
         return super.onOptionsItemSelected(item);
