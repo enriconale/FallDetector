f2bc7b144e1639f572ebd5fa16038d032731d491
enriconale <developer.nalettoe@yahoo.it>
2015-04-14 12:47:30 +0200

    Added RunningSessionService class

---
+++ app/src/main/java/it/unipd/dei/esp1415/thetrumannshow/FallDetector/RunningSessionService.java
@@ -0,0 +1,19 @@
+package it.unipd.dei.esp1415.thetrumannshow.FallDetector;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * @author Enrico Naletto
+ */
+public class RunningSessionService extends IntentService {
+
+    public RunningSessionService() {
+        super("RunningSessionService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
